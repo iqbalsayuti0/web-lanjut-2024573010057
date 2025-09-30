5ad2d84669792882be255f38bce179c747f712b0
iqbal <iqbalsayuti0@gmail.com>
2025-09-30 15:21:08 +0700

    30 sep 2025

---
+++ lab-group/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+Route::controller(PageController::class)->group(function () {
+    Route::get('/', 'home') ->name('home');
+    Route::get('/about', 'about') ->name('about');
+    Route::get('/contact', 'contact') ->name('contact');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
---
+++ project/helo-laravel/resources/views/mywelcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to laravel</title>
+</head>
+<body>
+    <h1>
+        Welcome to laravel 12
+    </h1>
+    <p>
+        Hello, {{$message}}
+    </p>
+</body>
+</html>
